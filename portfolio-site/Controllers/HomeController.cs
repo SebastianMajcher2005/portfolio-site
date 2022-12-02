e49b515e8dd08f85ede0830a6323b17d736b7ea4
Sebastian Majcher <SebastianMajcher2005@gmail.com>
2022-12-02 17:15:28 +0100

    Delete unnecessary files

--- portfolio-site/Controllers/HomeController.cs
+++ portfolio-site/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace portfolio_site.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
