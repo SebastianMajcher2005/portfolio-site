891060a1709c77d2d30fa09ff4d81426dc539d56
Sebastian Majcher <SebastianMajcher2005@gmail.com>
2022-12-04 14:29:01 +0100

    Add new animation to navbar while scrolling

--- portfolio-site/wwwroot/js/site.js
+++ portfolio-site/wwwroot/js/site.js
@@ -1,1 +1,19 @@
-﻿
+﻿window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+
+        document.getElementById("navbar").style.background = "#101010";
+        document.getElementById("my-name").style = "margin-top: 25px; margin-bottom: 25px; transition: all 0.5s ease-out;";
+    } else {
+
+        document.getElementById("navbar").style = "none";
+        document.getElementById("my-name").style = "margin-top: 50px; margin-bottom: 50px; transition: all 0.5s ease-out;";
+    }
+}
+
+const btn = document.getElementById('navbar-toggler');
+
+btn.addEventListener('click', function onClick() {
+        document.getElementById("navbar").style = "background: #101010;";
+});
