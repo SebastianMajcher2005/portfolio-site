3186435c10d203434f588802e9213fcb5eaa2044
Sebastian Majcher <SebastianMajcher2005@gmail.com>
2022-12-16 21:00:14 +0100

    Add Controller and view

---
+++ portfolio-site/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace portfolio_site.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
