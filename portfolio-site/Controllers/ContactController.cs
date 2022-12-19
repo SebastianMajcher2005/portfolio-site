f36a958a5480dd21b9d85156eb4933e2c9af8ade
Sebastian Majcher <SebastianMajcher2005@gmail.com>
2022-12-19 18:28:07 +0100

    Add method that send mails (DEMO)

--- portfolio-site/Controllers/ContactController.cs
+++ portfolio-site/Controllers/ContactController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentEmail.Core;
+using FluentEmail.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
 
 namespace portfolio_site.Controllers
 {
@@ -8,6 +12,29 @@ namespace portfolio_site.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> SendEmail()
+        {
+            var sender = new SmtpSender(() => new SmtpClient("smtp.gmail.com")
+            {
+                UseDefaultCredentials = false,
+                Port = 587,
+                Credentials = new NetworkCredential("sebastianmajcher2005@gmail.com", "fpyewhbthwypxpsu"),
+                EnableSsl = true,
+            });
+
+            Email.DefaultSender = sender;
+
+            var email = Email
+                .From("sebastianmajcher2005@gmail.com", "sebus")
+                .To("sebastianmajcher2005@gmail.com", "Seba")
+                .Subject("Thanks!")
+                .Body("hahahha");
+
+            await email.SendAsync();
+
+            return RedirectToAction("Index", "Contact");
         }
     }
 }
